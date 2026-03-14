9d4b7c995da1a37e0b6120ff7435f1453834299e
rkishony <rkishony@technion.ac.il>
2026-03-14 14:23:22 +0200

    env declering

--- env.py
+++ env.py
@@ -10,3 +10,5 @@ FIGURES_DIR.mkdir(exist_ok=True)
 
 RATE = 4000
 TYPES = ['TV', 'MV', 'PV', 'Phc', 'AV']
+CLASSES = [0, 1, 2, 3, 4]
+CATEGORY_NAMES = ['background', 'S1', 'systolic', 'S2', 'diastole']
