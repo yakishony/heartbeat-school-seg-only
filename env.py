0a738bd7294e021e97906669a6a05dc7c5c47ff1
rkishony <rkishony@technion.ac.il>
2026-04-10 14:52:00 +0300

    change downsampling factor to be 2

--- env.py
+++ env.py
@@ -3,7 +3,7 @@ from pathlib import Path
 PROJECT_DIR = Path(__file__).parent
 DATA_DOWNLOADED = Path.home() / "Downloads" / "the-circor-digiscope-phonocardiogram-dataset-1.0.3" / "training_data"
 DATA_RAW_DIR = PROJECT_DIR / "data_raw.pickle"
-DATA_FOR_ML = PROJECT_DIR / "downsampled_4x_data_for_ml"
+DATA_FOR_ML = PROJECT_DIR / "downsampled_2x_data_for_ml"
 
 
 FIGURES_DIR = PROJECT_DIR / "figures"
@@ -11,8 +11,9 @@ FIGURES_DIR = PROJECT_DIR / "figures"
 FIGURES_DIR.mkdir(exist_ok=True)
 
 RATE = 4000
-DOWNSAMPLE_FACTOR = 4
-RATE_DS = RATE // DOWNSAMPLE_FACTOR  # 1000
+DOWNSAMPLE_FACTOR = 2
+RATE_DS = RATE // DOWNSAMPLE_FACTOR  # 2000
+
 TYPES = ['TV', 'MV', 'PV', 'Phc', 'AV']
 CLASSES = [0, 1, 2, 3, 4]
 CATEGORY_NAMES = ['background', 'S1', 'systolic', 'S2', 'diastole']
